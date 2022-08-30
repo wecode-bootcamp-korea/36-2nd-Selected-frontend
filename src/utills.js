b32ca9161ff5a3bf780e76f0d4545a452a1d3d5a
Chaedie <chaedong.im.dev@gmail.com>
2022-08-30 16:34:22 +0900

    Refacotr: 코드 리뷰 반영

--- src/utills.js
+++ src/utills.js
@@ -1,8 +1,17 @@
-export const appendComma = number => {
-  return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
-};
-
 export const goToUrl = (navigate, url) => {
   navigate(url);
   window.scrollTo(0, 0);
+};
+
+export const request = async (uri, options = {}) => {
+  try {
+    const response = await fetch(uri, options);
+    if (response.ok) {
+      const json = await response.json();
+      return json;
+    }
+    throw new Error('API통신 실패 => response.ok :', response.ok);
+  } catch (e) {
+    console.error('API통신 실패 :', e.message);
+  }
 };
