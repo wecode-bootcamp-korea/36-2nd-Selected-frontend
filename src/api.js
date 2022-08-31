26af5ebbb65a84a3d047c3a6f431732f4c4bae62
소재현 <socowjh@gmail.com>
2022-08-31 10:09:16 +0900

    ADD: 채용페이지 무한스크롤 구현

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 const BASE_URL = 'http://10.58.4.73:3000';
 const BASE_URL2 = 'http://10.58.5.62:3000';
+const BASE_URL3 = 'http://10.58.6.6:3000';
 
 export const API = {
   UPLOAD_RESUME: `${BASE_URL}/resume/upload`,
   GET_RESUMES: `${BASE_URL}/resumes`,
   POST_LOGIN: `${BASE_URL2}/auths/kakao-login`,
+  GET_JOBS: `${BASE_URL3}/jobs/list`,
 };
---
+++ src/pages/RecruitList/RecruitList.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import * as S from './RecruitListStyled';
+import { BsFillBookmarkFill, BsChevronRight } from 'react-icons/bs';
+import { API } from '../../api';
+
+export default function RecruitList() {
+  const [list, setList] = useState([]);
+  const [scroll, setScroll] = useState(1);
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      setScroll(prev => prev + 1);
+    }
+  }, [inView]);
+
+  useEffect(() => {
+    fetch(`${API.GET_JOBS}?offset=0&limit=${scroll * 5}`)
+      .then(response => response.json())
+      .then(data => setList(data.jobs));
+  }, [scroll]);
+
+  return (
+    <S.RecruitList>
+      <S.BookMark>
+        <S.MarkButton>
+          <BsFillBookmarkFill size="20" />
+          <S.MarkText>북마크 모아보기</S.MarkText>
+          <BsChevronRight size="20" />
+        </S.MarkButton>
+      </S.BookMark>
+      {list.map(text => {
+        return (
+          <S.ListLi key={text.jobId}>
+            <S.ListLink>
+              <S.Header url={text.mainImageUrl} />
+              <S.ListTextBox>
+                <S.ListText size="23px">{text.companyName}</S.ListText>
+                <S.ListText size="18px">{text.department}</S.ListText>
+                <S.ListButton>
+                  <span>응답률 매우 높음</span>
+                </S.ListButton>
+                <S.ListSeoul>
+                  서울<span>.</span> 한국
+                </S.ListSeoul>
+                <S.ListMoney>채용보상금 1,000,000원</S.ListMoney>
+              </S.ListTextBox>
+            </S.ListLink>
+            <div ref={ref}>{inView.toString()}</div>
+          </S.ListLi>
+        );
+      })}
+    </S.RecruitList>
+  );
+}
