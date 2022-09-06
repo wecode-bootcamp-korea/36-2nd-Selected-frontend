e34be63594d0ff3ac18e0c8b7d288981701f1cbe
MoonHayden <hdmoon127@gmail.com>
2022-09-06 15:28:13 +0900

    fix
    fix

--- src/api.js
+++ src/api.js
@@ -1,14 +1,17 @@
 const BASE_URL = 'http://10.58.4.73:3000';
+const BASE_URL2 = 'http://10.58.5.62:3000';
+const BASE_URL4 = 'http://10.58.0.52:3000';
 const AWS_URL = process.env.REACT_APP_AWS_IP;
 
 export const API = {
   UPLOAD_RESUME: `${BASE_URL}/resume/upload`,
   GET_RESUMES: `${BASE_URL}/resumes/list`,
   GET_RESUME_DETAIL: `${BASE_URL}/resumes`,
-  POST_LOGIN: `${BASE_URL}/auths/kakao-login`,
   GET_JOBS: `${AWS_URL}/jobs/list`,
+  GET_JOBTAGS: `${BASE_URL4}/jobs/main?jobTags=2`,
   POST_ADD_RESUME: `${BASE_URL}/resumes/addresume`,
   POST_ADD_SKILL: `${BASE_URL}/resumes/addskill`,
   POST_ADD_CAREERS: `${BASE_URL}/resumes/addusercareer`,
   POST_ADD_LINK: `${BASE_URL}/resumes/addurl`,
+  POST_LOGIN: `${BASE_URL2}/auths/kakao-login`,
 };
